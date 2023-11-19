df70382e306ea7cc37947d6ca1887dcfccff2911
Tzuf <tzufar@gmail.com>
2023-11-19 11:40:45 +0200

    Update edit_distance_arabic.py

--- edit_distance_arabic/edit_distance_arabic.py
+++ edit_distance_arabic/edit_distance_arabic.py
@@ -4,6 +4,8 @@ This code calculates the weighted edit distance between two strings.
 
 import numpy as np
 
+PREFIX = [
+    'ال', 'ع','و','وال', 'لل', 'ل','ب', 'بال', 'عال', 'وع','ولل', 'ول', 'وب']
 
 COST_FREQ1 = 0.1
 DICT_LETTER_SWAP_FREQ1 = { # Dictionary of letter pairs with a substitution cost of COST_FREQ1 (0.1)
@@ -39,80 +41,138 @@ LIST_DICT_LETTER_SWAP_BY_FREQ = [
     (DICT_LETTER_SWAP_FREQ2, COST_FREQ2)
 ]
 
+class ArabicEditDistance():
+    def __init__(
+            self, 
+            insertion_cost=1, 
+            deletion_cost=1, 
+            substitution_cost=1,
+            list_dict_freq_costs=LIST_DICT_LETTER_SWAP_BY_FREQ,
+            ignore_prefix=False
+            ):
+        """
+        Args:
+            string1 (str): The first string.
+            string2 (str): The second string.
+            insertion_cost (int): The cost of inserting a character.
+            deletion_cost (int): The cost of deleting a character.
+            list_dict_freq_costs (list): list of tuples (dictionary, cost).
+            ignore_prefix: if 'True' then the edit distance will not consider optional prefixes.
 
-def weighted_letter_swap(
-        letter_1, letter_2, def_substitution_cost, list_dict_freq_costs):
-    """
-    Determines the cost of substituting one letter for another based on their frequency of substitution.
+        """
+        self.def_insertion_cost = insertion_cost
+        self.def_deletion_cost = deletion_cost
+        self.def_substitution_cost = substitution_cost
+        self.list_dict_freq_costs = list_dict_freq_costs
+        self.ignore_prefix = ignore_prefix
 
-    Args:
-        letter_1 (str): The first letter.
-        letter_2 (str): The second letter.
-        def_substitution_cost (float): the default substitution cost if not in the dictionaries.
-        list_dict_freq_costs (list): list of tuples: dictionary and cost.
+    def __weighted_letter_swap(
+            self, letter_1, letter_2):
+        """
+        Determines the cost of substituting one letter for another based on their frequency of substitution.
 
-    Returns:
-        int: The cost of substituting letter_1 for letter_2.
-    """
-    substitution_cost = def_substitution_cost
-    for dict_letter_swap, dict_cost in list_dict_freq_costs:
-        if letter_1 in dict_letter_swap and letter_2 in dict_letter_swap[letter_1]:
-            substitution_cost = dict_cost
-            break
-        elif letter_2 in dict_letter_swap and letter_1 in dict_letter_swap[letter_2]:
-            substitution_cost = dict_cost
-            break 
-    return substitution_cost
+        Args:
+            letter_1 (str): The first letter.
+            letter_2 (str): The second letter.
 
-def weighted_edit_distance(
-        string1, string2, insertion_cost=1, deletion_cost=1, def_substitution_cost=1, 
-        list_dict_freq_costs=LIST_DICT_LETTER_SWAP_BY_FREQ):
-    """
-    Calculates the weighted edit distance between two strings.
+        Returns:
+            int: The cost of substituting letter_1 for letter_2.
+        """
+        substitution_cost = self.def_substitution_cost
+        for dict_letter_swap, dict_cost in self.list_dict_freq_costs:
+            if letter_1 in dict_letter_swap and letter_2 in dict_letter_swap[letter_1]:
+                substitution_cost = dict_cost
+                break
+            elif letter_2 in dict_letter_swap and letter_1 in dict_letter_swap[letter_2]:
+                substitution_cost = dict_cost
+                break 
+        return substitution_cost
 
-    Args:
-        string1 (str): The first string.
-        string2 (str): The second string.
-        insertion_cost (int): The cost of inserting a character.
-        deletion_cost (int): The cost of deleting a character.
-        list_dict_freq_costs (list): list of tuples (dictionary, cost).
+    def get_edit_distance(
+            self, string1, string2):
+        """
+        Calculates the weighted edit distance between two strings.
 
-    Returns:
-        int: The weighted edit distance between the two strings.
-    """
-    n = len(string1)
-    m = len(string2)
+        Args:
+            string1 (str): The first string.
+            string2 (str): The second string.
+        Returns:
+            int: The weighted edit distance between the two strings.
+        """
+        min_weighted_distance = self.__weighted_edit_distance(
+            string1=string1, 
+            string2=string2)
+        if self.ignore_prefix:
+            return min_weighted_distance
+        
+        optional_pre_str1 = []
+        optional_pre_str2 = []
 
-    d = np.zeros((n + 1, m + 1))
+        for pre in PREFIX:
+            len_pre = len(pre)
+            if string1[:len_pre] == pre:
+                optional_pre_str1.append(string1[len_pre:])
+            if string2[:len_pre] == pre:
+                optional_pre_str2.append(string2[len_pre:]) 
 
-    # Initialize the first row with deletion costs
-    for i in range(1, n + 1):
-        d[i, 0] = deletion_cost * i
+        for string_no_pre1 in optional_pre_str1:    
+            for string_no_pre2 in optional_pre_str2:
 
-    # Initialize the first column with insertion costs
-    for j in range(1, m + 1):
-        d[0, j] = insertion_cost * j
+                weighted_distance = self.__weighted_edit_distance(
+                    string1=string_no_pre1, 
+                    string2=string_no_pre2)
+                
+                if weighted_distance<min_weighted_distance:
+                    min_weighted_distance = weighted_distance
+      
+        return min_weighted_distance
 
-    for i in range(1, n + 1):
+    def __weighted_edit_distance(
+            self, string1, string2):
+        """
+        Calculates the weighted edit distance between two strings.
+
+        Args:
+            string1 (str): The first string.
+            string2 (str): The second string.
+
+        Returns:
+            int: The weighted edit distance between the two strings.
+        """
+        n = len(string1)
+        m = len(string2)
+
+        d = np.zeros((n + 1, m + 1))
+
+        # Initialize the first row with deletion costs
+        for i in range(1, n + 1):
+            d[i, 0] = self.def_deletion_cost * i
+
+        # Initialize the first column with insertion costs
         for j in range(1, m + 1):
-            if string1[i - 1] == string2[j - 1]:
-                substitution_cost = 0
-            else:
-                substitution_cost = weighted_letter_swap(
-                    string1[i - 1], string2[j - 1], def_substitution_cost, list_dict_freq_costs)          
+            d[0, j] = self.def_insertion_cost * j
 
-            d[i, j] = min(d[i - 1, j] + deletion_cost,
-                          d[i, j - 1] + insertion_cost,
-                          d[i - 1, j - 1] + substitution_cost)
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if string1[i - 1] == string2[j - 1]:
+                    substitution_cost = 0
+                else:
+                    substitution_cost = self.__weighted_letter_swap(
+                        string1[i - 1], string2[j - 1])          
 
-    return d[n, m]
+                d[i, j] = min(d[i - 1, j] + self.def_deletion_cost,
+                            d[i, j - 1] + self.def_insertion_cost,
+                            d[i - 1, j - 1] + substitution_cost)
+
+        return d[n, m]
 
 
 if __name__ == "__main__":
-    string1 = "مؤسسة"
-    string2 = "موسسة"
+    string1 = "المؤسسة"
+    string2 = "عموسسة"
      
-    weighted_distance = weighted_edit_distance(string1, string2)
+    edit_distance = ArabicEditDistance()
+    weighted_distance = edit_distance.get_edit_distance(string1, string2)
     normalized_weighted_distance = weighted_distance/len(string2)
     
     print("Weighted edit distance:", weighted_distance)
